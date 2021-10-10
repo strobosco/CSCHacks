be32e14404e08a3dca87f779ddad0a6eaf4ec0ea
strobosco <54740672+strobosco@users.noreply.github.com>
2021-10-10 19:08:33 -0400

    Moved class CSS

---
+++ frontend/styles/globals/globals.js
@@ -0,0 +1,57 @@
+export const globals = {
+  "*": {
+    fontFamily: "Gill Sans",
+  },
+  ".nav-bar": {
+    width: "80%",
+    mt: 8,
+    mx: "auto",
+    position: "sticky",
+    zIndex: "1",
+    backgroundColor: "secondary",
+    borderRadius: "6px",
+    top: "0",
+    padding: "5",
+    flexDirection: "row",
+    color: "white",
+  },
+  ".intro-text": {
+    h2: {
+      color: "white",
+      fontSize: "24px",
+      fontWeight: "bold",
+    },
+    p: {
+      color: "white",
+      fontSize: "18px",
+      pt: "8px",
+    },
+  },
+  ".vertical-stack": {
+    width: "full",
+    height: "full",
+    padding: 5,
+    bg: "secondary",
+    color: "white",
+    alignItems: "flex-start",
+    borderRadius: "8px",
+  },
+  ".intro-container": {
+    mt: 10,
+    mx: "auto",
+    px: 0,
+    width: "80%",
+    maxWidth: "100%",
+  },
+  ".footer-container": {
+    mt: 8,
+    p: 8,
+    width: "80%",
+    mx: "auto",
+    bg: "secondary",
+    color: "white",
+    maxWidth: "100%",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.5)",
+    borderRadius: "8px",
+  },
+};
