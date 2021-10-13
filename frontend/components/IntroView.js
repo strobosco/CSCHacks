f54d90c39cbdba5657ed095234fd4a1817f2890f
strobosco <54740672+strobosco@users.noreply.github.com>
2021-10-13 18:48:07 -0400

    Updated view

--- frontend/components/IntroView.js
+++ frontend/components/IntroView.js
@@ -2,21 +2,25 @@ import React from "react";
 
 import { Container, Flex, VStack } from "@chakra-ui/react";
 
+import InputGrid from "./InputGrid";
+
 const IntroView = () => {
   return (
     <Container p={0} maxW="100%" width="80%" className="intro-container">
       <Flex>
-        <VStack spacing={10} className="vertical-stack">
+        <VStack
+          alignItems="flex-start"
+          spacing={10}
+          className="vertical-stack left-stack"
+        >
           <div className="intro-text">
             <h2>Spotify Playlist Clustering</h2>
-            <p>
-              This simple web app utilizes the Spotify Web API to retrieve a
-              users liked songs and create an amount of playlists chosen by the
-              user.
-            </p>
+            <p>ADD DESCRIPTION</p>
           </div>
         </VStack>
-        <VStack spacing={10} className="vertical-stack"></VStack>
+        <VStack alignItems="flex-start" spacing={10} className="vertical-stack">
+          <InputGrid />
+        </VStack>
       </Flex>
     </Container>
   );
