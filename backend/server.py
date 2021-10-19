ede891b638622e756763ece6782b8e625c4229b0
strobosco <54740672+strobosco@users.noreply.github.com>
2021-10-19 17:33:46 -0400

    Backend

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from dotenv import load_dotenv
+load_dotenv()
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/cluster")
-# run the clustering
+@app.route("/cluster", methods=['POST'])
+def cluster():
+  data = request.json
+  print(data['data'])
+  #testlets see
+  return "Request received"
+
+"""
+Returned data will be:
+  new playlists to be displayed 
+
+Data passed in:
+  access token
+  list of songs (FULL CSV STYLE DATA)
+  number of clusters
+  root name
+"""
